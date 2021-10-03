5d0741704f853ba9de098494998c90719a508636
DeveloperBuilder <35078547+DeveloperBuilder@users.noreply.github.com>
2021-10-03 11:07:08 +0200

    Add files via upload

---
+++ HTML.js
@@ -0,0 +1,36 @@
+console.log("Hello Winc Academy")
+
+let name = "Developer";
+console.log(name);
+
+/*6. het is niet mogelijk om een variabele aan te maken met de naam if, 
+want dat is een gereserveerde trefwoord net als 'let'.*/
+
+/*7. je kan alleen nummer in string optellen, 
+mits je de haakjes gebruikt*/
+let sum = 4 + 4;
+console.log("sum:", sum);
+let sumWithStrings = "4" + "4";
+console.log("sum with strings:", sumWithStrings);
+
+//8. simpele sommetjes
+let addition = 5 + 3;
+console.log("addition", addition);
+let subtraction = 20 - 2;
+console.log("subtraction", subtraction);
+let multiplication = 10 * 10;
+console.log("multiplication", multiplication);
+let division = 9 / 3;
+console.log("division", division);
+let modulus = 12 % 9;
+console.log("modulus", modulus);
+
+/*9.Maak een variabele met je leeftijd in cijfers (bijvoorbeeld 48), 
+log* het type variabele naar de console met typeof (typeof operator)*/
+let age = 48;
+console.log(typeof age);
+
+/*10. Verander nu het type van deze variable door je leeftijd ("48") 
+uit te schrijven in "text", log het type naar de console met typeof*/
+let ageAsString = "48";
+console.log(typeof ageAsString)
